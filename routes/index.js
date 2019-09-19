c11d759f54d69075129063e07a9f0e70bc5f5bc4
Md Tanjil Shivan <salwadhaka@gmail.com>
2019-09-19 10:11:13 +0200

    change in styling

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ const router = express.Router();
 const Restaurant = require('./restaurant')
 const Review = require('../models/Review')
 const Contact = require('../models/Contact')
-// também adicionei essa parte
-const url = require('url');
-//acaba aqui
 
 const context = "Zmxvdy1pZD00NDliNzFmMi05NGIwLTUzMDItOWNmNC1mYjllNWE3ZTA4ZjJfMTU2ODc1Nzc4MDg3OV8yNTMyXzU4OTcmcmFuaz0xOQ";
 
@@ -16,44 +13,58 @@ router.get('/', (req, res, next) => {
 });
 
 const appId = process.env.APP_ID,  // .env file theke client id ar secret nicche
-  appCode = process.env.APP_CODE;
+appCode = process.env.APP_CODE;
 
 /* GET search page */
 // get all the restaurants in SERACH 
 router.get("/restaurantlist", (req, res, next) => {
   let queryParameter = req.query.search; // can be restaurant 
+  let category = "eat-drink";
   // console.log(queryParameter);
   axios.get(`https://places.cit.api.here.com/places/v1/discover/search?app_id=${appId}&app_code=${appCode}&at=52.5206,13.3889&q=${queryParameter}`)
-    .then(rest => {
-      // mudanças começam aqui
-      rest.data.results.items.forEach(item => {
-        const href = url.parse(item.href);
-        console.log(href);
-        item.id = href.pathname.substring(href.pathname.lastIndexOf("/") + 1);
-      });
-      // terminam aqui
-      res.render("restaurantList.hbs", { restaurantList: rest.data.results.items });
-    }).catch(err => console.log(err))
+      .then(rest => {
+        rest.data.results.items.forEach(el=>el.vicinity = el.vicinity.split("<br/>").join(", "))
+    res.render("restaurantList.hbs", { restaurantList: rest.data.results.items });
+  }).catch(err=>console.log(err))
 });
 
 
 // // get a unique page for each restaurant by id
 router.get("/restaurants/:restaurantID", (req, res) => {
-  const query = req.params.restaurantID;
-  // troquei a url pra esssa /places/${query}
-  axios.get(`https://places.cit.api.here.com/places/v1/places/${query}?app_id=${appId}&app_code=${appCode}`)
-    .then(rest => {
-      res.render("restaurantDetail.hbs", { restaurantDetail: rest.data });
-    }).catch(err => console.log(err))
+const query = req.params.restaurantID;
+console.log('query:' + query)
 
 // axios.get(`https://places.cit.api.here.com/places/v1/discover/search?app_id=${appId}&app_code=${appCode}&at=52.5206,13.3889&q=${query}`)
 axios.get(`https://places.cit.api.here.com/places/v1/places/${query};context=${context}?app_id=${appId}&app_code=${appCode}`)
 .then(rest => {
-        // console.log('promise:',rest)
-    res.render("restaurantDetail.hbs", { restaurantDetail: rest.data });
+  res.render("restaurantDetail.hbs", { restaurantDetail: rest.data });
+  console.log('promise:',rest.data.location.position)
   }).catch(err=>console.log(err))
    
  });
+
+ // access to map
+router.get("/restaurants/:restaurantID", (req, res) => {
+  const query = req.params.restaurantID;
+  console.log('query:' + query)
+  
+  // axios.get(`https://places.cit.api.here.com/places/v1/discover/search?app_id=${appId}&app_code=${appCode}&at=52.5206,13.3889&q=${query}`)
+  axios.get(`https://places.cit.api.here.com/places/v1/places/${query};context=${context}?app_id=${appId}&app_code=${appCode}`)
+  .then(rest => {
+    let coordinate = rest.data.location.position;
+    console.log('promise:',coordinate)
+    res.render("restaurantDetail.hbs", { restaurantDetail: rest.data });
+    }).catch(err=>console.log(err))
+     
+   });
+//  const val1 = 52.5233702;
+// const val2 = 13.3535779;
+
+// const val3 = 52.5233702;
+// const val4 = 13.3535779;
+
+
+
 router.get('/review', (req, res, next) => {
   res.render('review');
 });
@@ -97,5 +108,6 @@ router.post('/contact', (req, res, next) => {
 router.get('/game', (req, res, next) => {
   res.render('game');
 });
+
 
 module.exports = router;
