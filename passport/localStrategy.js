6f35bc087b4710cb5bcda762b1af494b0a5b7b18
Juliana Ferreira Bastos <julferbas@yahoo.com.br>
2019-09-19 13:26:14 +0200

    url correction

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -64,8 +64,8 @@ passport.use(new GitHubStrategy({
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "localhost:3000/auth/facebook/callback"
-  // "https://hungryhackers.herokuapp.com/auth/facebook/callback"
+  callbackURL: "https://hungryhackers.herokuapp.com/auth/facebook/callback"
+  //"localhost:3000/auth/facebook/callback"
 },
   (accessToken, refreshToken, profile, done) => {
     // find a user with profile.id as facebookId or create one
